23d6aefab22368a7a92ba2482213b466c736e532
ruochen1112 <q.ruochen@gmail.com>
2017-11-19 02:45:24 -0600

    load second dataset

--- script.js
+++ script.js
@@ -7,51 +7,82 @@ var height = 400;
 
 
 
-d3.json("data.json", function(error, data) {
-
-    dataset = data;
+ d3.json("data.json", function(error,data) {
+        dataset = data;
     
-});
+
     creatmap();
 
-    creatbar();
+   
 
+});
 
-
-
+  d3.json("gang.json", function(error,data) {
+        gang = data;
+          creatbar();
+});
 
 
 
 function creatmap() {
 
-    var svg = d3.select("#chart")
+
+    var map = L.map('map').setView([41.88, -87.63], 12);
+        mapLink = 
+            '<a href="http://openstreetmap.org">OpenStreetMap</a>';
+        L.tileLayer(
+            'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; ' + mapLink + ' Contributors',
+            maxZoom: 18,
+            }).addTo(map);
+
+        map._initPathRoot()    
+
+    var svg = d3.select("#map")
     .append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom);
 
-  var g = svg.append('g')
+    var g = svg.append('g')
     .attr("transform", "translate(" + margin.left*2 + "," + margin.top*2 + ")");
+
+    var points = selectAll("circle")
+        .data(dataset.Location)
+        .enter().append("svg:circle")
+        .attr("r", 10)
+        .attr("cx", function(d){return map(d.geometry.coordinates)[0];})
+        .attr("cy", function(d){return map(d.geometry.coordinates)[1];})
+
+
+
+
+
+
+    };
 
 
 
 
                     
-};
+
 
 
 function creatbar() {
 
-    var svg = d3.select("#chart")
-    .append("svg")
+var svg = d3.select("#bar").append("svg")
     .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom);
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
 
   var g = svg.append('g')
     .attr("transform", "translate(" + margin.left*2 + "," + margin.top*2 + ")");
 
+};
 
 
 
                     
-};
+
 
