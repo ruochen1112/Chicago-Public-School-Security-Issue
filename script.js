87799e901e8e785b04679ec28ba0fe211ca494f7
ruochen1112 <q.ruochen@gmail.com>
2017-12-08 22:09:51 -0600

    mixed content issue

--- script.js
+++ script.js
@@ -19,9 +19,12 @@ var yAxis;
 function creatmap() {
 
     map = L.map('map',{zoomControl: false}).setView([41.88, -87.63], 12);
-        mapLink = L.tileLayer('http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png', {
+        mapLink = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}.{ext}', {
+  attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+  subdomains: 'abcd',
+  minZoom: 0,
   maxZoom: 20,
-  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+  ext: 'png'
 }).addTo(map);
 
  
